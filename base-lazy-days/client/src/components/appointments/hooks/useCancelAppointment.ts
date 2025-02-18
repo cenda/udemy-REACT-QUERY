8c4fbd92014f8656e59b39682e71afd3409d9cc9
Čenda Kovář <cenda.kovar@gmail.com>
2025-02-18 08:16:11 +0100

    feat: implement data mutation on server

--- base-lazy-days/client/src/components/appointments/hooks/useCancelAppointment.ts
+++ base-lazy-days/client/src/components/appointments/hooks/useCancelAppointment.ts
@@ -3,20 +3,32 @@ import { Appointment } from "@shared/types";
 import { axiosInstance } from "@/axiosInstance";
 import { useCustomToast } from "@/components/app/hooks/useCustomToast";
 import { queryKeys } from "@/react-query/constants";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 // for when server call is needed
-// async function removeAppointmentUser(appointment: Appointment): Promise<void> {
-//   const patchData = [{ op: 'remove', path: '/userId' }];
-//   await axiosInstance.patch(`/appointment/${appointment.id}`, {
-//     data: patchData,
-//   });
-// }
+async function removeAppointmentUser(appointment: Appointment): Promise<void> {
+  const patchData = [{ op: "remove", path: "/userId" }];
+  await axiosInstance.patch(`/appointment/${appointment.id}`, {
+    data: patchData,
+  });
+}
 
 export function useCancelAppointment() {
+  const queryClient = useQueryClient();
   const toast = useCustomToast();
 
-  // TODO: replace with mutate function
-  return (appointment: Appointment) => {
-    // nothing to see here
-  };
+  const { mutate } = useMutation({
+    // mutationFn: (appointment: Appointment) =>
+    //   removeAppointmentUser(appointment),
+
+    // !!! there is no need to pass variable in anonymous function, because it's the same as called function param
+    // !!! that's the difference from useReserveAppointment, where we have to pass the user ID as well
+    mutationFn: removeAppointmentUser,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: [queryKeys.appointments] });
+      toast({ title: "You've cancelled your appointment!" });
+    },
+  });
+
+  return mutate;
 }
